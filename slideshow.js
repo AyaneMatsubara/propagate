ed81455492092989a06e53d8d9403e4b733b4685
AyaneMatsubara <ayataka912nerv@gmail.com>
2018-09-21 16:23:02 +0900

    first commit

---
+++ slideshow.js
@@ -0,0 +1,16 @@
+(function($){
+
+  $(document).ready(function(){
+    var slides = $("#slideshow > li");
+    var count = 0;
+
+    function toggle_slide(){
+      count = (count + 1) % 4;
+      slides.removeClass("current").eq(count).addClass("current");
+    }
+    
+    setInterval(toggle_slide, 10000);
+
+  });
+
+})(jQuery);
